9ea9238f412903b523b90a0e164b3842609def7a
YoungAuthorj <youngauthorjozef@yahoo.com>
2023-01-22 08:29:55 -0800

    AirBnB_clone_v2

---
+++ models/__init__.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+"""This module instantiates an object of class FileStorage"""
+
+from models.engine.file_storage import FileStorage
+
+
+
+
+
+storage = FileStorage()
+
+storage.reload()
